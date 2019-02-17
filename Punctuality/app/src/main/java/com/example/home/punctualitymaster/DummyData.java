110a50d00a963502770d9e838d4231c465a43a27
Atheonix <parkergmusic@gmail.com>
2019-02-16 16:27:42 -0800

    created a ListView adapter for our options

---
+++ Punctuality/app/src/main/java/com/example/home/punctualitymaster/DummyData.java
@@ -0,0 +1,8 @@
+package com.example.home.punctualitymaster;
+
+public class DummyData {
+
+    public static String[] options = {"Favorite", "Saved", "Create", "Browse"};
+
+    public static String[] option_data = {"Here would be a list of favorites", "Here would be a list of Saved", "this would be its own a activity/fragment for creating/editing", "here would be a list pulled from the interwebs of other schedules"};
+}
